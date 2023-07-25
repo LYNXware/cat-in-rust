f27710383c8d6cf24097f1a838b21ab26dde741e
Ben <benphawke@gmail.com>
2023-07-25 16:34:49 +0200

    Refactor/cleanup (#16)
    * println! -> log::info!
    
    * Init function/helper items
    
    * revert init helper
    
    * re-scope the initialisation section
    
    * main event-loop helper
    
    * Organise imports
    
    * minor-cleanup
    
    * Add a whole heap of trace logs

--- src/board_modules/left_finger.rs
+++ src/board_modules/left_finger.rs
@@ -1,21 +1,25 @@
 use embedded_hal::digital::v2::{InputPin, OutputPin};
 use esp_backtrace as _;
 use hal::gpio::{AnyPin, Input, Output, Pins, PullUp, PushPull};
+
 use keyberon::key_code::KeyCode;
 use keyberon::matrix::Matrix;
 use keyberon::{action::k, debounce::Debouncer, layout::Layers};
 
-use KeyCode::*;
-
 #[rustfmt::skip]
-static LAYOUT: Layers<6, 4, 1> = [[
+static LAYOUT: Layers<6, 4, 1> = {
+#[allow(clippy::enum_glob_use)]
+use KeyCode::*;
+[[
     [k(No), k(No), k(Escape), k(LCtrl), k(LAlt), k(No)],
     [k(No), k(Q),  k(W),      k(E),     k(R),    k(T)],
     [k(A),  k(S),  k(D),      k(F),     k(G),    k(Q)],
     [k(Z),  k(X),  k(C),      k(V),     k(B),    k(R)],
-]];
+]] 
+};
 
 /// 4 rows, 6 columns per row, just the one layer
+#[allow(clippy::module_name_repetitions)]
 pub struct BoardLeftFinger<C: InputPin, R: OutputPin> {
     pub matrix: Matrix<C, R, 4, 6>,
     pub debouncer: Debouncer<[[bool; 4]; 6]>,
@@ -44,9 +48,16 @@ impl BoardLeftFinger<AnyPin<Input<PullUp>>, AnyPin<Output<PushPull>>> {
         .unwrap();
         let debounce = || [[false; 4]; 6];
 
+        let debouncer = Debouncer::new(debounce(), debounce(), 50);
+        log::trace!("debounce: 50");
+        log::trace!("matrix created: pull-up input:    [21, 47,48, 45]");
+        log::trace!("matrix created: push-pull-output: [37, 38, 39, 40, 41, 42]");
+
+        log::trace!("layout: \n {:?}", LAYOUT);
+
         Self {
             matrix,
-            debouncer: Debouncer::new(debounce(), debounce(), 50),
+            debouncer,
             layout: LAYOUT,
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,66 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_println::println;
+use esp_println::logger::init_logger;
 use hal::prelude::*;
 use hal::{clock::ClockControl, peripherals::Peripherals, timer::TimerGroup, Rtc, IO};
 
+use keyberon::layout::Event;
+
 mod board_modules;
 use board_modules::left_finger;
-use keyberon::layout::Event;
 
 #[entry]
 fn main() -> ! {
-    // Obtain board resources
-    let peripherals = Peripherals::take();
-    let mut system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let io = {
+        init_logger(log::LevelFilter::Trace);
+        log::trace!("entered main, logging initialized");
+        // Obtain board resources
+        let peripherals = Peripherals::take();
+        log::trace!("Peripherals claimed");
+        let mut system = peripherals.SYSTEM.split();
+        log::trace!("System components claimed");
+        let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+        log::trace!("clocks claimed with boot-defaultes, and frozen");
 
-    // Disable the RTC and TIMG watchdog timers
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(
-        peripherals.TIMG0,
-        &clocks,
-        &mut system.peripheral_clock_control,
-    );
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(
-        peripherals.TIMG1,
-        &clocks,
-        &mut system.peripheral_clock_control,
-    );
-    let mut wdt1 = timer_group1.wdt;
-    rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
+        // Disable the RTC and TIMG watchdog timers
+        let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+        log::trace!("Rtc claimed");
+        let timer_group0 = TimerGroup::new(
+            peripherals.TIMG0,
+            &clocks,
+            &mut system.peripheral_clock_control,
+        );
+        log::trace!("TimerGroup0 created");
+        let mut wdt0 = timer_group0.wdt;
+        let timer_group1 = TimerGroup::new(
+            peripherals.TIMG1,
+            &clocks,
+            &mut system.peripheral_clock_control,
+        );
+        log::trace!("TimerGroup1 created");
+        let mut wdt1 = timer_group1.wdt;
+        log::trace!("Disabling timers:");
+        rtc.rwdt.disable();
 
+        log::trace!("\t rtc.rwdt.disable()");
+        wdt0.disable();
+        log::trace!("\t wdt0.disable()");
+        wdt1.disable();
+        log::trace!("\t wdt1.disable()");
+
+        IO::new(peripherals.GPIO, peripherals.IO_MUX)
+    };
+    log::trace!("gpio created");
+    main_loop(io)
+}
+
+fn main_loop(io: IO) -> ! {
     // Setup the board-left-finger module
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut board_left_finger = left_finger::BoardLeftFinger::new(io.pins);
 
     // Demonstrate PoC using keyberon to read the matrix
-    println!("Begin reading keyboard");
+    log::info!("Begin reading keyboard");
     for _ in 0.. {
         let events = board_left_finger
             .debouncer
@@ -52,11 +73,11 @@ fn main() -> ! {
             .collect::<heapless::Vec<_, 8>>();
         for event in events.iter() {
             match event {
-                Event::Press(x, y) => println!(
+                Event::Press(x, y) => log::info!(
                     "P-{:?}",
                     board_left_finger.layout[0][*x as usize][*y as usize]
                 ),
-                Event::Release(x, y) => println!(
+                Event::Release(x, y) => log::info!(
                     "R-{:?}",
                     board_left_finger.layout[0][*x as usize][*y as usize]
                 ),
