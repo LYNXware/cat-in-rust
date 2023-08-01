0cef2beab14ec75a97304500287279715c4279ab
Ben PHL <benphawke@gmail.com>
2023-08-01 14:15:14 +0200

    feed keyboard reports over usb, log over uart

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,25 @@
 
 use esp_backtrace as _;
 use esp_println::logger::init_logger;
-use hal::prelude::*;
-use hal::{clock::ClockControl, peripherals::Peripherals, timer::TimerGroup, Rtc, IO};
+use hal::otg_fs::{UsbBus, USB};
+use hal::{
+    clock::ClockControl,
+    peripherals::Peripherals,
+    timer::TimerGroup,
+    uart::{config::Config as UartConfig, TxRxPins as UartTxRx, Uart},
+    Rtc, IO,
+};
+use hal::{prelude::*, Delay};
 
 use keyberon::layout::Event;
 
 mod board_modules;
 use board_modules::left_finger;
+use usb_device::prelude::{UsbDeviceBuilder, UsbVidPid};
+use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor, MouseReport};
+use usbd_hid::hid_class::HIDClass;
+
+static mut USB_MEM: [u32; 1024] = [0; 1024];
 
 #[entry]
 fn main() -> ! {
@@ -51,32 +63,52 @@ fn main() -> ! {
     log::trace!("\t wdt1.disable()");
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-    log::trace!("gpio created");
-    // Setup the board-left-finger module
-    let mut board_left_finger = left_finger::BoardLeftFinger::new(io.pins);
+    let uart_vdd_pin = io.pins.gpio1;
+    let mut uart_vdd_pin = uart_vdd_pin.into_push_pull_output();
+    uart_vdd_pin.set_high().unwrap();
 
-    // Demonstrate PoC using keyberon to read the matrix
-    log::info!("Begin reading keyboard");
-    for _ in 0.. {
-        let events = board_left_finger
-            .debouncer
-            .events(
-                board_left_finger.matrix.down_keys().unwrap(),
-                Some(keyberon::debounce::transpose),
-            )
-            .collect::<heapless::Vec<_, 8>>();
-        for event in events.iter() {
-            match event {
-                Event::Press(x, y) => log::info!(
-                    "P-{:?}",
-                    board_left_finger.layout[0][*x as usize][*y as usize]
-                ),
-                Event::Release(x, y) => log::info!(
-                    "R-{:?}",
-                    board_left_finger.layout[0][*x as usize][*y as usize]
-                ),
-            }
-        }
+    let t_r_pins = UartTxRx::new_tx_rx(io.pins.gpio5, io.pins.gpio6);
+    let mut uart = Uart::new_with_config(
+        peripherals.UART0,
+        Some(UartConfig::default()),
+        Some(t_r_pins),
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
+    log::info!("uart-setup");
+    let mut delay = Delay::new(&clocks);
+
+    let usb = USB::new(
+        peripherals.USB0,
+        io.pins.gpio18,
+        io.pins.gpio19,
+        io.pins.gpio20,
+        &mut system.peripheral_clock_control,
+    );
+    let usb_bus = UsbBus::new(usb, unsafe { &mut USB_MEM });
+
+    let mut hid = HIDClass::new(&usb_bus, KeyboardReport::desc(), 1);
+    for _ in 0..5 {
+        uart.write_bytes(b".");
+        delay.delay_ms(500u32);
     }
-    unreachable!()
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0, 0))
+        .manufacturer("shady-bastard")
+        .product("totally not a malicious thing")
+        .device_class(3)
+        .build();
+
+    let mut i = 2000;
+    let down = &[0,0,0,0,0,0,0,0];
+    let up = &[0,0,20,0,0,0,0,0];
+    loop {
+        if !usb_dev.poll(&mut [&mut hid]) {
+            continue;
+        }
+
+        let rep = if i % 2000 < 1000 { down} else {up};
+        i += 1;
+        let _res = hid.push_raw_input(rep);
+    }
 }
