c2d6f1926a78d2c65c69947f534b91fc7a565fac
Ben PHL <benphawke@gmail.com>
2023-09-25 18:04:58 +0200

    Constrain key-state read-buf to a bit-aware len

--- components/src/matrix.rs
+++ components/src/matrix.rs
@@ -1,11 +1,12 @@
 use crate::ReadState;
-use core::ops::Mul;
+use core::ops::{Mul, Add, Div};
 use hal::{
     blocking::delay::DelayUs,
     digital::v2::{InputPin, OutputPin},
 };
 
 use generic_array::{ArrayLength, GenericArray};
+use typenum::{U7, U8};
 
 /// Pin container in uninitialized form
 #[allow(non_upper_case_globals)]
@@ -55,10 +56,29 @@ impl<
         D: DelayUs<u16>,
     > ReadState for KeyDriver<InP, OutP, InN, OutN, D>
 where
-    <InN as Mul<OutN>>::Output: ArrayLength,
+    // first, get the raw size of the array
+    InN: Mul<OutN>,
+    // Now, we need to div-by-8 to get the number of bytes needed for all the bits...
+    // 
+    // Without addinng 7, the last byte (if partially needed) is cut-out (4 * 5 -> 25. 25/8 -> 4,
+    // losing the remainder).
+    // This 7 ensures that we do not lose the remainder...
+    // By dividing from the next multiple of 8, We solve that problem...
+    // Consider an array of 8 by 8 = 64. Add the 7, it will be 71/8 = 64/8 under integer division...
+    // BUT: an array of 6 by 7 = 42. div-by-8 has the 2-remainder lost.
+    // Add 7, we have (42 + 7 = 49) / 8 = 6. The lost remainder is unwanted. 
+    <InN as Mul<OutN>>::Output: Add<U7>,
+    // ...and we do the division
+    <<InN as Mul<OutN>>::Output as Add<U7>>::Output: Div<U8>,
+    // ...and tell the compiler it must impl array length. that becomes our LEN
+    <<<InN as Mul<OutN>>::Output as Add<U7>>::Output as Div<U8>>::Output: ArrayLength,
+    // Why not just use const-generics? That requires a nightly feature to do arithmetic in the
+    // implementation constraints, and even so, the compiler warns that feature is incomplete, and
+    // can cause panics
 {
+
     // TODO: this is currently bit_len, but it bytes, not bits :(
-    type LEN = <InN as Mul<OutN>>::Output;
+    type LEN = <<<InN as Mul<OutN>>::Output as Add<U7>>::Output as Div<U8>>::Output;
     fn read_state(&mut self, buf: &mut GenericArray<u8, Self::LEN>) {
         self.matrix.read_state(buf, &mut self.delayer)
     }
