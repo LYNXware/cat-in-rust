5e581f2edaf817af3a51656d1dd28fe30ae39d5e
Ben PHL <benphawke@gmail.com>
2023-09-26 15:43:50 +0200

    refine right-finger layout

--- configs/src/right_finger.rs
+++ configs/src/right_finger.rs
@@ -6,9 +6,9 @@ pub static LAYERS: Layers<6, 4, 1> = {
 #[allow(clippy::enum_glob_use)]
 use KeyCode::*;
 [[
-    [k(No), k(No), k(Escape), k(LCtrl), k(LAlt), k(T)],
-    [k(No), k(Q),  k(W),      k(E),     k(R),    k(T)],
-    [k(A),  k(S),  k(D),      k(F),     k(G),    k(Q)],
-    [k(Z),  k(X),  k(C),      k(V),     k(B),    k(R)],
+    [k(X),  k(Escape), k(LCtrl), k(LAlt), k(No), k(No)],
+    [k(No), k(Y),      k(U),     k(I),    k(O),  k(P)],
+    [k(No), k(H),      k(J),     k(K),    k(L),  k(M)],
+    [k(No), k(X),      k(C),     k(V),    k(B),  k(R)],
 ]] 
 };
